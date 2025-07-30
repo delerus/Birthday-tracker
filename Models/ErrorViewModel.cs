e636c8f1a4fdd26a31f5d56c1eb1e06020cb67bb
delerus <delerus21@gmail.com>
2025-07-30 13:58:37 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Birthday_tracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
