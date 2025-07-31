43b5c0d5773a754eefd1d8bbc097e3f662a9322d
delerus <delerus21@gmail.com>
2025-07-31 11:45:40 +0300

    Connected to MSSQL and created migration

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Birthday_tracker.Models;
+
+namespace Birthday_tracker.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public IConfiguration _config { get; set; }
+
+        public ApplicationDbContext(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_config.GetConnectionString("DatabaseConnection"));
+        }
+
+        public DbSet<Birthday> Birthdays { get; set; }
+    }
+}
