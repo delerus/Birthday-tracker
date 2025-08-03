604b121dd9ac19c548f94bd10477107e812b0099
delerus <delerus21@gmail.com>
2025-08-03 18:18:03 +0300

    Added new model for home page view

---
+++ Models/NearBirthdaysViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Birthday_tracker.Models
+{
+    public class NearBirthdaysViewModel
+    {
+        public IEnumerable<Birthday> UpcomingBirthdays { get; set; }
+        public IEnumerable<Birthday> MonthlyBirthdays { get; set; }
+    }
+}
